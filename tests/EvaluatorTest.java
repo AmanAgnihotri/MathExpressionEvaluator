34ec4852e9543262d807693d1c7697ea64736364
shkesar <shubhamkesar@live.com>
2014-03-02 10:23:02 +0530

    Another test and that's good

--- tests/EvaluatorTest.java
+++ tests/EvaluatorTest.java
@@ -13,6 +13,14 @@ public class EvaluatorTest {
     public void testExpressions() {
         double expVal = 3.0;
         double resVal = MathExpressionEvaluator.evaluateExpression("-4 + 7");
-        assertEquals(resVal, expVal, 0.0);
+        assertEquals(expVal, resVal, 0.0);
+
+        expVal = -8.0;
+        resVal = MathExpressionEvaluator.evaluateExpression("-1 + 3 -(4 * 3 - 3 + (3 / 3))");
+        assertEquals(expVal, resVal, 0.0);
+
+        expVal = 0.0;
+        resVal = MathExpressionEvaluator.evaluateExpression("-");
+        System.out.format("%d", resVal);
     }
 }
