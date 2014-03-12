1a3800f4d3f1717f95dfb8707850bbd1fc030b82
Aman Agnihotri <awfully.awesome@yahoo.com>
2014-03-12 11:13:53 +0530

    Improved Code
    Improved code performance by using StringBuilder for appending
    characters, and by removing some redundancies in the code.

--- evaluator/MathExpressionEvaluator.java
+++ evaluator/MathExpressionEvaluator.java
@@ -8,80 +8,88 @@ public class MathExpressionEvaluator
   {
     Stack<Double> operandStack = new Stack<Double>();
     Stack<Character> operatorStack = new Stack<Character>();
-    
+
     expression = insertBlanks(expression);
-    
-    String tokens[] = expression.split(" ");
-    
+
+    String tokens[] = expression.split("\\s+");
+
     for (String token : tokens)
     {
       if (token.length() == 0)
+      {
         continue;
-      else if (token.charAt(0) == '+' || token.charAt(0) == '-')
-      {
-        while (!operatorStack.isEmpty()
-            && isValidOperator(operatorStack.peek()))
-        {
-          processAnOperator(operandStack, operatorStack);
-        }
-        
-        operatorStack.push(token.charAt(0));
-      }
-      else if (token.charAt(0) == '*' || token.charAt(0) == '/')
-      {
-        while (!operatorStack.isEmpty()
-            && (operatorStack.peek() == '*' || operatorStack.peek() == '/'))
-        {
-          processAnOperator(operandStack, operatorStack);
-        }
-        operatorStack.push(token.charAt(0));
-      }
-      else if (token.trim().charAt(0) == '(')
-      {
-        operatorStack.push('(');
-      }
-      else if (token.trim().charAt(0) == ')')
-      {
-        while (operatorStack.peek() != '(')
-        {
-          processAnOperator(operandStack, operatorStack);
-        }
-        operatorStack.pop();
       }
       else
       {
-        operandStack.push(new Double(token));
+        double ch = token.charAt(0);
+        if (ch == '+' || ch == '-')
+        {
+          while (!operatorStack.isEmpty()
+            && isValidOperator(operatorStack.peek()))
+          {
+            processAnOperator(operandStack, operatorStack);
+          }
+
+          operatorStack.push(ch);
+        }
+        else if (ch == '*' || ch == '/')
+        {
+          while (!operatorStack.isEmpty()
+            && (operatorStack.peek() == '*' || operatorStack.peek() == '/'))
+          {
+            processAnOperator(operandStack, operatorStack);
+          }
+          operatorStack.push(ch);
+        }
+        else if (ch == '(')
+        {
+          operatorStack.push('(');
+        }
+        else if (ch == ')')
+        {
+          while (operatorStack.peek() != '(')
+          {
+            processAnOperator(operandStack, operatorStack);
+          }
+          operatorStack.pop();
+        }
+        else
+        {
+          operandStack.push(new Double(token));
+        }
       }
     }
-    
+
     while (!operatorStack.isEmpty())
     {
       processAnOperator(operandStack, operatorStack);
     }
     return operandStack.pop();
   }
-  
-  private static boolean isValidOperator(Character operator)
+
+  private static boolean isValidOperator(char operator)
   {
     return operator == '+' || operator == '-' || operator == '*'
-        || operator == '/';
+      || operator == '/';
   }
-  
+
   private static void processAnOperator(Stack<Double> operandStack,
-      Stack<Character> operatorStack)
+                                        Stack<Character> operatorStack)
   {
     char operand = operatorStack.pop();
     double operatorA = operandStack.pop();
-    
+
     if (operandStack.isEmpty())
     {
       if (operand == '-')
+      {
         operandStack.push(-operatorA);
+      }
       return;
     }
-    
+
     double operatorB = operandStack.pop();
-    
+
     switch (operand)
     {
       case '+':
@@ -98,19 +106,23 @@ public class MathExpressionEvaluator
         break;
     }
   }
-  
+
   private static String insertBlanks(String s)
   {
-    String result = "";
+    StringBuilder result = new StringBuilder();
     for (int i = 0; i < s.length(); i++)
     {
       char c = s.charAt(i);
-      
+
       if (c == '(' || c == ')' || c == '+' || c == '-' || c == '*' || c == '/')
-        result += " " + s.charAt(i) + " ";
+      {
+        result.append(" ").append(c).append(" ");
+      }
       else
-        result += s.charAt(i);
+      {
+        result.append(c);
+      }
     }
-    return result;
+    return result.toString();
   }
 }
